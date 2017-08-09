4cfe294088ab4b8667f68bd16c2a8f7635ffcd0d
Prateek55 <prateekvij55@gmail.com>
2017-08-09 16:23:16 -0400

    Added sample code for loading dataset

---
+++ lib/sample_load_dataset.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+"""
+The file contains the example code for loading the dataset given the csv file
+"""
+from dataset import Dataset
+
+__author__ = "Prateek Vij"
+__copyright__ = "Copyright 2017, Carnegie Mellon University"
+__credits__ = ["Amir Zadeh", "Prateek Vij", "Soujanya Poria"]
+__license__ = "GPL"
+
+
+# Arguments for Dataset class
+csv_fpath = "../configs/CMU_MOSI_all.csv"
+timestamps = "absolute" # absolute or relative, relative will output features relative to segment time 
+
+# Code for loading
+d = Dataset(csv_fpath, timestamps=timestamps)
+features = d.load()
+
+# View modalities
+print d.modalities # Modalities are numbered as modality_0, modality_1, ....
+
+# View features for a particular segment of a modality
+modality = "modality_1" # replace 0 with 1, 2, .... for different modalities
+video_id = '_dI--eQ6qVU' # example video_id
+segment_id = '2' # sample segment_id
+
+for feat in features[modality][video_id][segment_id]:
+	print feat # tuples of form (start_time, end_time, feat_val)
+
+
+
+
+
