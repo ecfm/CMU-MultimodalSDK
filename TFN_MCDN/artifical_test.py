5c0f7cff58bff360f8a89eebf0f7de6a9f9771a5
var95 <var6595@gmail.com>
2017-10-16 00:32:58 -0400

    Updated TFN_MCDN Model

--- TFN_MCDN/artifical_test.py
+++ TFN_MCDN/artifical_test.py
@@ -5,38 +5,43 @@ from torch.autograd import Variable
 from MCDN_TFN_Model_With_Different_LSTM_Dim import MCDN
 from TFN_Model import Tensor_Fusion_Network
 
+#Add config file parameters for each param mentioned below
 input_dim = 2
 time = 3
 modality = 3
 input_seq = []
 target = Variable(torch.FloatTensor([3.5]))
+epochs = 10
+learning_rate = 0.1
+nattentions = 6
+optimizer = ["SGD","Adam"]
 
+for e in range(epochs):
 
+	for t in range(time):
+	    step = []
+	    for m in range(modality):
+	        step.append(Variable(torch.randn(1, input_dim)))	#Input_Data from MOSI
+	    input_seq.append(step)
 
-for t in range(time):
-    step = []
-    for m in range(modality):
-        step.append(Variable(torch.randn(1, input_dim)))
-    input_seq.append(step)
+	mcdn = MCDN([2,2,2], [20,20,20], 15, nattentions)
 
-mcdn = MCDN([2,2,2], [20,20,20], 15, 6)
+	final_layer = nn.Linear(15, 1)
 
-final_layer = nn.Linear(15, 1)
+	loss = nn.MSELoss()
 
-loss = nn.MSELoss()
+	optimizer = optim.SGD(mcdn.parameters(), lr=learning_rate)
+	
+	mcdn.zero_grad()
 
-optimizer = optim.SGD(mcdn.parameters(), lr=0.1)
+	(hidden, zx) = mcdn(input_seq)
+	output = final_layer(zx)
 
-mcdn.zero_grad()
+	loss_val = loss(output, target)
+	print loss_val
 
-(hidden, zx) = mcdn(input_seq)
-output = final_layer(zx)
+	loss_val.backward()
+	optimizer.step()
 
-loss_val = loss(output, target)
-print loss_val
-
-loss_val.backward()
-optimizer.step()
-
-#Hyperparameters being used: 
+#Hyperparameters being used: SGD learning rate, Time Steps, Epochs, no. of attentions
 # writer.writerow([])
