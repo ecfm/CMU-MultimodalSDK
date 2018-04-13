ab75f54f0b179fb3f947436bc75f4a013f9f0492
Justin1904 <justin1904@live.com>
2018-04-13 01:24:12 -0400

    if sub datasets don't have modalities info we'll still allow the merge

--- mmdata/dataset.py
+++ mmdata/dataset.py
@@ -634,11 +634,14 @@ class Dataset(object):
         assert isinstance(dataset1, Dataset)
         assert isinstance(dataset2, Dataset)
         # merge the feature_dict and modalities attributes
-        merged_modalities = Dataset.merge_dict(dataset1.modalities, dataset2.modalities)
+        try:
+            merged_modalities = Dataset.merge_dict(dataset1.modalities, dataset2.modalities)
+            mergedDataset.modalities = merged_modalities
+        except:
+            pass
         merged_feat_dict = Dataset.merge_dict(dataset1.feature_dict, dataset2.feature_dict)
         mergedDataset = Dataset()
         mergedDataset.feature_dict = merged_feat_dict
-        mergedDataset.modalities = merged_modalities
         return mergedDataset
 
 
