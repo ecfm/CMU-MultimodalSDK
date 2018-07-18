fc6793004bf17337bfe71b01f4fc3de66deef08b
Amir Zadeh <abagherz@cs.cmu.edu>
2018-07-18 15:25:47 -0400

    facet visual

--- mmsdk/mmdatasdk/dataset/standard_datasets/CMU_MOSI/cmu_mosi.py
+++ mmsdk/mmdatasdk/dataset/standard_datasets/CMU_MOSI/cmu_mosi.py
@@ -3,4 +3,4 @@ raw["words"]='http://immortal.multicomp.cs.cmu.edu/CMU-MOSI/language/CMU_MOSI_Ti
 raw["phonemes"]='http://immortal.multicomp.cs.cmu.edu/CMU-MOSI/language/CMU_MOSI_TimestampedPhones.csd'
 highlevel={}
 highlevel["glove_vectors"]='http://immortal.multicomp.cs.cmu.edu/CMU-MOSI/language/CMU_MOSI_TimestampedWordVectors.csd'
-highlevel["FACET 4.1"]='http://immortal.multicomp.cs.cmu.edu/CMU-MOSI/language/CMU_MOSI_VisualFacet_4.1.csd'
+highlevel["FACET 4.1"]='http://immortal.multicomp.cs.cmu.edu/CMU-MOSI/visual/CMU_MOSI_VisualFacet_4.1.csd'
