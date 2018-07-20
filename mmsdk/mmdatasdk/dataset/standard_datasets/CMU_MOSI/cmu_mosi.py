036be3a56de6aae99fce0bd5fbf3d348ee124572
Amir Zadeh <abagherz@cs.cmu.edu>
2018-07-20 16:12:39 -0400

    mosi labels

--- mmsdk/mmdatasdk/dataset/standard_datasets/CMU_MOSI/cmu_mosi.py
+++ mmsdk/mmdatasdk/dataset/standard_datasets/CMU_MOSI/cmu_mosi.py
@@ -1,6 +1,10 @@
 raw={}
 raw["words"]='http://immortal.multicomp.cs.cmu.edu/CMU-MOSI/language/CMU_MOSI_TimestampedWords.csd'
 raw["phonemes"]='http://immortal.multicomp.cs.cmu.edu/CMU-MOSI/language/CMU_MOSI_TimestampedPhones.csd'
+#frames and pcm values coming soon
 highlevel={}
 highlevel["glove_vectors"]='http://immortal.multicomp.cs.cmu.edu/CMU-MOSI/language/CMU_MOSI_TimestampedWordVectors.csd'
 highlevel["FACET 4.1"]='http://immortal.multicomp.cs.cmu.edu/CMU-MOSI/visual/CMU_MOSI_VisualFacet_4.1.csd'
+#covarep and opensmile to be added
+labels={}
+labels["Opinion Segment Labels"]="http://immortal.multicomp.cs.cmu.edu/CMU-MOSI/labels/CMU_MOSI_Opinion_Labels.csd"
