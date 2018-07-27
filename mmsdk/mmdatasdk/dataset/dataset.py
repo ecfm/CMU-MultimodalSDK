f55fe19145e681e58f5aee5a153456b45d50bf9a
Amir Zadeh <abagherz@cs.cmu.edu>
2018-07-26 20:55:03 -0400

    align

--- mmsdk/mmdatasdk/dataset/dataset.py
+++ mmsdk/mmdatasdk/dataset/dataset.py
@@ -1,5 +1,10 @@
 from mmsdk.mmdatasdk import log, computational_sequence
 import sys
+import numpy
+import time
+from tqdm import tqdm
+
+epsilon=10e-5
 
 class mmdataset:
 	def __init__(self,recipe,destination=None):
@@ -20,10 +25,92 @@ class mmdataset:
 		for entry,compseq in self.computational_sequences.iteritems():
 			compseq.bib_citations(outfile)
 
-	#TODO: update these with the fast implementation
-	def align():
-		pass
+	#TODO: this is implentation #1. update with new implentation later 
+	def align(self,reference,replace=True):
+		aligned_output={}
+		for sequence_name in self.computational_sequences.keys():
+			aligned_output[sequence_name]={}
+		if reference not in self.computational_sequences.keys():
+			log.error("Computational sequence %s does not exist in dataset"%reference,error=True)
+		refseq=self.computational_sequences[reference].data
+		#this for loop is for entry_key - for example video id or the identifier of the data entries
+		log.status("Alignment based on %s computational sequence started ..."%reference)
+		pbar = tqdm(total=len(refseq.keys()),unit=" Computational Sequence Entries")
+		pbar.set_description("Overall Progress")
+		for entry_key in list(refseq.keys()):
+			pbar_small=tqdm(total=refseq[entry_key]['intervals'].shape[0])
+			pbar_small.set_description("Aligning %s"%entry_key)
+			#intervals for the reference sequence
+			for i in range(refseq[entry_key]['intervals'].shape[0]):
+				ref_time=refseq[entry_key]['intervals'][i,:]
+				if (abs(ref_time[0]-ref_time[1])<epsilon):
+					pbar_small.update(1)
+					continue
+				#aligning all sequences (including ref sequence) to ref sequence
+				for otherseq_key in list(self.computational_sequences.keys()):
+					otherseq=self.computational_sequences[otherseq_key].data[entry_key]
+					#list to contain intersection for (otherseq_key,i)
+					list_intervals=[]
+					list_features=[]
+					#checking all intervals of the otherseq for intersection
+					for j in range(otherseq["intervals"].shape[0]):
+						sub_time=otherseq["intervals"][j]
+						this_features=otherseq["features"][j,:]
+						intersect,intersect_start,intersect_end=self.__intersect(ref_time,sub_time)
+						if intersect == True:
+							list_intervals.append([intersect_start,intersect_end])
+							list_features.append(this_features)
+					
+					aligned_output[otherseq_key][entry_key+"[%d]"%i]={}
+					aligned_output[otherseq_key][entry_key+"[%d]"%i]["intervals"]=numpy.array(list_intervals,dtype='float32')
+					aligned_output[otherseq_key][entry_key+"[%d]"%i]["features"]=numpy.array(list_features,dtype='float32')
+					if (len(aligned_output[otherseq_key][entry_key+"[%d]"%i]["intervals"].shape)!=2):
+						print ("Fuck")
+						print (aligned_output[otherseq_key][entry_key+"[%d]"%i]["intervals"].shape)
+						print (aligned_output[otherseq_key][entry_key+"[%d]"%i]["features"].shape)
+						print (ref_time,i)
+						print (refseq[entry_key]['features'][i,:].shape)
+						time.sleep(10)
+				pbar_small.update(1)
+			pbar_small.visible=False
+			pbar_small.close()
+			pbar.update(1)
+		pbar.visible=False
+		pbar.close()
+		log.success("Alignment to %s done."%reference)
+		if replace is True:
+			log.status("Replacing dataset content with aligned computational sequences")
+			self.__set_computational_sequences(aligned_output)
+			return None
+		else:
+			log.status("Creating new dataset with aligned computational sequences")
+			newdataset=mmdataset({})
+			newdataset.__set_computational_sequences(aligned_output)
+			return newdataset	
+		print()
 	
+	def __set_computational_sequences(self,new_computational_sequences_data):
+		self.computational_sequences={}
+		for sequence_name in list(new_computational_sequences_data.keys()):
+			self.computational_sequences[sequence_name]=computational_sequence(sequence_name)
+			self.computational_sequences[sequence_name].setData(new_computational_sequences_data[sequence_name],sequence_name)
+				
+
+	def __intersect(self,ref_time,sub_time):
+		s0,e0=ref_time
+		s1,e1=sub_time
+		#intersection
+		if e1-s0>=epsilon and e0-s1>=epsilon:
+			intersect_start=max(s0,s1)
+			intersect_end=min(e0,e1)
+			#just boundary case
+			if intersect_start==intersect_end:
+				return False,None,None
+			return True,intersect_start,intersect_end
+		#no intersection
+		else:
+			return False,None,None
 	def unify():
 		pass
+
 	
--- mmsdk/mmdatasdk/dataset/standard_datasets/CMU_MOSI/cmu_mosi.py
+++ mmsdk/mmdatasdk/dataset/standard_datasets/CMU_MOSI/cmu_mosi.py
@@ -6,7 +6,6 @@ highlevel={}
 highlevel["glove_vectors"]='http://immortal.multicomp.cs.cmu.edu/CMU-MOSI/language/CMU_MOSI_TimestampedWordVectors.csd'
 highlevel["FACET 4.1"]='http://immortal.multicomp.cs.cmu.edu/CMU-MOSI/visual/CMU_MOSI_VisualFacet_4.1.csd'
 highlevel["OpenSmile-emobase2010"]='http://immortal.multicomp.cs.cmu.edu/CMU-MOSI/acoustic/CMU_MOSI_OpenSmile.csd'
-highlevel["OpenFace V1.0"]='http://immortal.multicomp.cs.cmu.edu/CMU-MOSI/visual/CMU_MOSI_OpenFace_V1.csd'
 #covarep to be added
 labels={}
 labels["Opinion Segment Labels"]="http://immortal.multicomp.cs.cmu.edu/CMU-MOSI/labels/CMU_MOSI_Opinion_Labels.csd"
