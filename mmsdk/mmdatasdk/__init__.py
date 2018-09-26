59c2468d6a4c25b9a9b6ab39e590f4611a8b3f95
Amir Zadeh <abagherz@cs.cmu.edu>
2018-09-26 11:23:07 -0400

    added pom

--- mmsdk/mmdatasdk/__init__.py
+++ mmsdk/mmdatasdk/__init__.py
@@ -4,3 +4,5 @@ from mmsdk.mmdatasdk.dataset.standard_datasets.CMU_MOSEI import cmu_mosei as cmu
 from mmsdk.mmdatasdk.dataset.standard_datasets.CMU_MOSEI import cmu_mosei_std_folds as cmu_mosei_std_folds
 from mmsdk.mmdatasdk.dataset.standard_datasets.CMU_MOSI import cmu_mosi as cmu_mosi
 from mmsdk.mmdatasdk.dataset.standard_datasets.CMU_MOSI import cmu_mosi_std_folds as cmu_mosi_folds
+from mmsdk.mmdatasdk.dataset.standard_datasets.POM import pom as pom
+#from mmsdk.mmdatasdk.dataset.standard_datasets.CMU_MOSI import cmu_mosi_std_folds as cmu_mosi_folds
