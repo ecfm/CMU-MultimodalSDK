ca07c4d620e9b95a63fa378902cf7426f2e85a7f
Amir Zadeh <abagherz@cs.cmu.edu>
2018-10-09 15:44:03 -0400

    Update download_dataset.py

--- examples/download_dataset.py
+++ examples/download_dataset.py
@@ -16,9 +16,10 @@ parser.add_argument('dataset',
 
 args = parser.parse_args()
 choice={"cmu_mosei":mmdatasdk.cmu_mosei.highlevel,"cmu_mosi":mmdatasdk.cmu_mosi.highlevel,"pom":mmdatasdk.pom.highlevel}
-
+labels={"cmu_mosei":mmdatasdk.cmu_mosei.labels,"cmu_mosi":mmdatasdk.cmu_mosi.labels,"pom":mmdatasdk.pom.labels}
 
 dataset=mmdatasdk.mmdataset(choice[args.dataset],'./downloaded_dataset')
+dataset.add_computational_sequence(labels[args.dataset],'./downloaded_dataset')
 
 print ("List of the computational sequences in the downloaded dataset")
 print (dataset.computational_sequences.keys())
