1d876e498c0a1d26c41cbffa490f0b0190bf5b00
Amir Zadeh <abagherz@cs.cmu.edu>
2018-12-31 13:58:53 -0500

    mmmodelsdk cleanup

--- mmsdk/mmmodelsdk/fusion/multiple_attention/model.py
+++ mmsdk/mmmodelsdk/fusion/multiple_attention/model.py
@@ -26,6 +26,9 @@ class MultipleAttentionFusion(nn.Module):
 		self.dim_reduce_nets=dim_reduce_nets
 		self.num_atts=num_atts
 
+	def __call__(self,in_modalities):
+		return self.fusion(in_modalities)
+
 	def fusion(self,in_modalities):
 
 		#getting some simple integers out
@@ -51,7 +54,7 @@ class MultipleAttentionFusion(nn.Module):
 		#apply the dim_reduce
 		dim_reduced=[self.dim_reduce_nets[i](out_modalities[i]) for i in range(num_modalities)]
 		#multiple attention done :)
-		return dim_reduced
+		return dim_reduced,out_modalities
 
         def forward(self, x):
 		print("Not yet implemented for nn.Sequential")
@@ -61,5 +64,31 @@ class MultipleAttentionFusion(nn.Module):
 
 if __name__=="__main__":
 	print("This is a module and hence cannot be called directly ...")
-	exit(-1)
+	print("A toy sample will now run ...")
+	
+	from torch.autograd import Variable
+	import torch.nn.functional as F
+	import numpy
+
+	inputx=Variable(torch.Tensor(numpy.array(numpy.zeros([32,40]))),requires_grad=True)
+	inputy=Variable(torch.Tensor(numpy.array(numpy.zeros([32,12]))),requires_grad=True)
+	inputz=Variable(torch.Tensor(numpy.array(numpy.zeros([32,20]))),requires_grad=True)
+	modalities=[inputx,inputy,inputz]
+
+	#simple functions for toy example, 4 times extract attentions hence 72*4
+	my_attention =	nn.Sequential(nn.Linear(72,72*4))
+	small_netx =	nn.Sequential(nn.Linear(160,10))
+	small_nety =	nn.Sequential(nn.Linear(48,20))
+	small_netz =	nn.Sequential(nn.Linear(80,30))
+
+	smalls_nets=[small_netx,small_nety,small_netz]
+
+	fmodel=MultipleAttentionFusion(my_attention,smalls_nets,4)	
+	out=fmodel(modalities)
+
+	print("Output")
+	print(out[0])
+	print("Toy sample finished ...")
+
+
 
