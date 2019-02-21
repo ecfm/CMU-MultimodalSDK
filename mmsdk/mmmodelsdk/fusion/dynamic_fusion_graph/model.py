3af8420cfa1b300209c39a63b3c15aea9501def1
KuzmaNg <36065074+KuzmaNg@users.noreply.github.com>
2019-02-21 10:21:12 +0800

    Update model.py

--- mmsdk/mmmodelsdk/fusion/dynamic_fusion_graph/model.py
+++ mmsdk/mmmodelsdk/fusion/dynamic_fusion_graph/model.py
@@ -24,7 +24,7 @@ from collections import OrderedDict
 class DynamicFusionGraph(nn.Module):
 
     def __init__(self,pattern_model,in_dimensions,out_dimension,efficacy_model):
-                super(DynamicFusionGraph, self).__init__()
+		super(DynamicFusionGraph, self).__init__()
 
 		self.num_modalities=len(in_dimensions)
 		self.in_dimensions=in_dimensions
