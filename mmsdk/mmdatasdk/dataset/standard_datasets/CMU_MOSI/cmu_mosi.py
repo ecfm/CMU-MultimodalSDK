e4dc9cf82c22c6f5100e613d6e82c0be27a2bad1
Amir Zadeh <abagherz@cs.cmu.edu>
2019-04-04 12:55:12 -0400

    Update cmu_mosi.py

--- mmsdk/mmdatasdk/dataset/standard_datasets/CMU_MOSI/cmu_mosi.py
+++ mmsdk/mmdatasdk/dataset/standard_datasets/CMU_MOSI/cmu_mosi.py
@@ -1,12 +1,14 @@
 raw={}
-raw["words"]='http://immortal.multicomp.cs.cmu.edu/CMU-MOSI/language/CMU_MOSI_TimestampedWords.csd'
+raw["CMU_MOSI_ModifiedTimestampedWords"]='http://immortal.multicomp.cs.cmu.edu/CMU-MOSI/language/CMU_MOSI_TimestampedWords.csd'
 raw["phonemes"]='http://immortal.multicomp.cs.cmu.edu/CMU-MOSI/language/CMU_MOSI_TimestampedPhones.csd'
-#frames and pcm values coming soon
+
 highlevel={}
 highlevel["BERT embeddings"]='http://immortal.multicomp.cs.cmu.edu/CMU-MOSI/language/CMU_MOSI_TimestampedBERT.csd'
 highlevel["glove_vectors"]='http://immortal.multicomp.cs.cmu.edu/CMU-MOSI/language/CMU_MOSI_TimestampedWordVectors.csd'
 highlevel["FACET 4.1"]='http://immortal.multicomp.cs.cmu.edu/CMU-MOSI/visual/CMU_MOSI_VisualFacet_4.1.csd'
-highlevel["OpenSmile-emobase2010"]='http://immortal.multicomp.cs.cmu.edu/CMU-MOSI/acoustic/CMU_MOSI_OpenSmile.csd'
+highlevel["OpenSmile-emobase2010"]='http://immortal.multicomp.cs.cmu.edu/CMU-MOSI/acoustic/CMU_MOSI_OpenSmile_EB10.csd'
+highlevel["OpenSMILE"]='http://immortal.multicomp.cs.cmu.edu/CMU-MOSI/acoustic/CMU_MOSI_openSMILE_IS09.csd'
+highlevel["OpenFace"]='http://immortal.multicomp.cs.cmu.edu/CMU-MOSI/acoustic/CMU_MOSI_OpenFace2.csd'
 highlevel["COVAREP"]='http://immortal.multicomp.cs.cmu.edu/CMU-MOSI/acoustic/CMU_MOSI_COVAREP.csd'
 #covarep to be added
 labels={}
