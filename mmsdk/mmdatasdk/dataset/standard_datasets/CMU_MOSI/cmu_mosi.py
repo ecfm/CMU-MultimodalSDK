322c7ee189f4a96c5ace425d56602fd3934ba17f
Amir Zadeh <abagherz@cs.cmu.edu>
2019-06-22 20:15:47 -0400

    Update cmu_mosi.py

--- mmsdk/mmdatasdk/dataset/standard_datasets/CMU_MOSI/cmu_mosi.py
+++ mmsdk/mmdatasdk/dataset/standard_datasets/CMU_MOSI/cmu_mosi.py
@@ -7,7 +7,7 @@ highlevel={}
 #but make sure BERT is extracted per segment and not per video. Otherwise your method will have more information 
 #beyond just segment. 
 #highlevel["BERT embeddings"]='http://immortal.multicomp.cs.cmu.edu/CMU-MOSI/language/CMU_MOSI_TimestampedBERT.csd'
-#highlevel["glove_vectors"]='http://immortal.multicomp.cs.cmu.edu/CMU-MOSI/language/CMU_MOSI_TimestampedWordVectors.csd'
+highlevel["glove_vectors"]='http://immortal.multicomp.cs.cmu.edu/CMU-MOSI/language/CMU_MOSI_TimestampedWordVectors_1.1.csd'
 highlevel["FACET 4.1"]='http://immortal.multicomp.cs.cmu.edu/CMU-MOSI/visual/CMU_MOSI_VisualFacet_4.1.csd'
 highlevel["OpenSmile-emobase2010"]='http://immortal.multicomp.cs.cmu.edu/CMU-MOSI/acoustic/CMU_MOSI_OpenSmile_EB10.csd'
 highlevel["OpenSMILE"]='http://immortal.multicomp.cs.cmu.edu/CMU-MOSI/acoustic/CMU_MOSI_openSMILE_IS09.csd'
